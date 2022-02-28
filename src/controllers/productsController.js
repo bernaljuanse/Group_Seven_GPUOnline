c9387377de9fa41522c8ba5dde2a38589c36e132
SebasBernal <jusebemas@gmail.com>
2022-02-28 13:10:40 -0500

    Sprint 5 parte 2

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,38 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 const { redirect } = require("express/lib/response");
 
-module.exports = {
+
+
+const controller = {
     listar: (req,res)=> {
-        let products =[
-            {id:1 , name:"tarjeta 1 gamer ", description: "para jugar", imagen:"/img/tarjeta1", price:400},
-            {id:2 , name:"tarjeta 2 streaming ", description: "para streaming" , imagen:"/img/tarjeta2", price:500},
-            {id:3 , name:"tarjeta 3 streaming and gaming ", description: "para streaming and jugar", imagen:"/img/tarjeta3", price:900},
-            {id:4 , name:"tarjeta 1 gamer ", description: "para jugar", imagen:"/img/tarjeta1", price:400},
-            {id:5 , name:"tarjeta 2 streaming ", description: "para streaming", imagen:"/img/tarjeta2", price:9595959}
-        ]
-        res.render("./products/listaProducto.ejs", {"products":products})
-        /* res.render("./products/listaProducto.ejs") */
+
+        res.render("./products/listaProducto.ejs", { products })
     },
-    vista: (req,res)=> {
+    vistaCrear: (req,res)=> {
         res.render("./products/crearProducto.ejs")
     },
     crear: (req,res)=> {
-        //res.send(req.body)
-        
             //tambien podemos todo lo que contiene: req.body
             //si existe crear el nombre requerir file.name y si no poner un nombre por defecto
             let image = req.file ? req.file.filename : "no existe";
-            console.log(req.file.filename);
-            
-            let productos = {
-                nombre: req.body.name,
-                descripcion: req.body.description,
-                precio: req.body.price,
 
+            let newProduct = {
+                id: products[products.length - 1].id + 1,
+                ... req.body,
+                image
             }
-       
+            products.push(newProduct)
+            fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "))
         // Campo para Guardar informacion del formulario crear
 
-        res.redirect("/listaProducto") //pensar lo que vamos a redireccionar
+        res.redirect("/products") //pensar lo que vamos a redireccionar
     },
     editar: (req,res)=> {
         
@@ -55,6 +54,8 @@ module.exports = {
         //res.redirect("/listaProducto") //pensar lo que vamos a redireccionar
     },
 
-    
+
 
 };
+
+module.exports =  controller;
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
-
 const productsController = require('../controllers/productsController');
 
 // ****** inicio de multer *******
 let storage = multer.diskStorage({
-    destination: (req, res, cb) => cb(null, "public/img/products"),
-    filename: (req, res, cb) => cb(null, Date.now() + "-" + file.originalname)
+    destination: (req, file, cb) => cb(null, "public/img/products"),
+    filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
 });
 
 //si var storage = multer.disk... se llamaria var pepe = multer.disk... entonces var upload = multer({storage:pepe})
@@ -15,9 +14,17 @@ let upload = multer({storage});
 
 const { route } = require('express/lib/application');
 
-router.get("/listaProducto",productsController.listar); /* listado de producto */
-router.get("/crearProducto",productsController.vista); /* Formulario de creación de productos. solo visualiza crearProducto.ejs */
+router.get("/",productsController.listar); /* listado de producto */
+
+// TAREA, implementar el metodo de detalle para la vista del cliente.
+/* router.get("/detalle/:id", productsController.detail); */
+
+router.get("/crearProducto",productsController.vistaCrear); /* Formulario de creación de productos. solo visualiza crearProducto.ejs */
+
 router.post("/crearProducto", upload.single("image"),productsController.crear); /* Acción de creación. "crea producto" con form y redirecciona a listaProducto.ejs */
+
+
+
 router.get("/editar/:idProduct",productsController.editar); /* Formulario de edición de productos */
 router.put("/editar",function(req,res) {
     res.send("fui por put")
